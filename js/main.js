e1b0b93dda25e3919c97b276d7d7fa802ae0fa3a
reinaldo422 <reinaldo422@gmail.com>
2016-04-30 14:26:38 -0400

    added methods to close menu when link cliked

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    // Onclick listener to change colors on navigation menu
+    function hamburgerClick() {
+        var hamburger = document.getElementById("hamburger");
+        var clicked = hamburger.getAttribute("clicked");
+        var iconBars = document.querySelectorAll(".icon-bar");
+        var navBar = document.querySelector(".navbar");
+
+        if (clicked === "false") {
+            hamburger.setAttribute("clicked", "true");
+            for (var i = 0; i < iconBars.length; i++) {
+                iconBars[i].style.backgroundColor = '#fff';
+            }
+            navBar.style.background = "rgba(0,0,0,0.25)"
+        } else {
+            hamburger.setAttribute("clicked", "false");
+            setTimeout(function(){
+                for (var i = 0; i < iconBars.length; i++) {
+                    iconBars[i].style.backgroundColor = '#2c3e50';
+                }
+                navBar.style.background = "rgba(0,0,0,0)"
+            }, 300);
+        }
+    }
+
+    // Collapse menu when link is clicked
+    function hamburgerCollapse() {
+        $('.collapse').collapse('hide')
+        hamburgerClick()       
+    }
+
+    document.getElementById("hamburger").onclick = hamburgerClick;
+    document.getElementById("nav-jumps").onclick = hamburgerCollapse;
+
+
+    // Smooth scrolling for links
+    $('#nav-jumps').localScroll({duration:800});
+});
